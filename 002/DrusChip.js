f25ff53c31b89ac5aa33a124485006f1c079ab6f
Javier Fernandez <drusunlimited@gmail.com>
2021-11-16 23:59:55 +0100

    DrusChip

--- 002/DrusChip.js
+++ 002/DrusChip.js
@@ -1,86 +1,17 @@
 import {LitElement, html, css, unsafeCSS} from 'lit';
+import componentStyles from './DrusChip-styles';
 
 export class DrusChip extends LitElement{
 
-    static styles=css`
-
-        :host{
-
-            display:inline-block;
-            text-transform:upperCase;
-            cursor:default;
-
-            --font-size:16px;
-            --padding:0.3em 0.8em;
-            --background-color:#cc1111;
-            --color:black;
-
-            font-size:var(--font-size);
-            padding:var(--padding);
-            background-color:var(--background-color);
-            color:var(--color);
-                
-        }
-
-        :host([shadow]){
-            box-shadow:0 2px 6px #00000066;
-        }
-
-
-        :host([rounded]){
-            border-radius:4px;
-        }
-
+    static get styles(){
+        return componentStyles;
+    };
     
-        :host([size="S"]){
-            --font-size:12px;
-        }
-        
-        :host([size="L"]){
-            --font-size:20px;
-        }
-        
-        :host([size="M"]){
-            /* fall in default */
-        }
-
-        :host([size="XL"]){
-            --font-size:24px;
-        }
-
-        :host([color="primary"]){
-            --background-color:#cc1111;
-            --color:white;
-        }
-
-        :host([color="secondary"]){
-            --background-color:#212121;
-            --color:white;
-        }
-
-        :host([color="alert"]){
-            --background-color:#cc9900;
-            --color:white;
-        }
-
-        :host([color="success"]){
-            --background-color:#0e8331;
-            --color:white;
-        }
-
-        /* STATE */
-
-        :host([disabled]){
-            opacity:0.4;
-            filter:grayscale(100);
-            pointer-events:none;
-        }
-
-
-    `;
-/* 
+    /* 
     static properties={
-    } */
+    
+    } 
+    */
 
     constructor(){
         super();
@@ -88,9 +19,7 @@ export class DrusChip extends LitElement{
 
 
     render(){
-        return html`
-            <span><slot></slot></span>
-        `;
+        return html`<span><slot></slot></span>`;
     }
 
 }
